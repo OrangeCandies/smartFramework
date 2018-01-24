021fa29da599d1db3a3e611f27c1471cb3929523
OrangeCandies <liuhui.mail@foxmail.com>
2018-01-24 14:29:51 +0800

    完成了输入流的转换和URL的编解码

---
+++ src/main/java/framework/bean/Data.java
@@ -0,0 +1,13 @@
+package framework.bean;
+
+public class Data {
+
+    private Object model;
+
+    public Data(Object model){
+        this.model = model;
+    }
+    public Object getModel() {
+        return model;
+    }
+}
